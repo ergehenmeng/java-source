14029af2e02d6aa6cd7c99ad39916c62bae05715
殿小二 <dianxiaoer@mandofin.com>
2021-03-12 09:10:53 +0800

    优化代码

--- src/com/eghm/ClassHeadTest.java
+++ src/com/eghm/ClassHeadTest.java
@@ -3,12 +3,29 @@ package com.eghm;
 import org.openjdk.jol.info.ClassLayout;
 
 public class ClassHeadTest {
-    public static void main(String[] args) {
-        User user = new User();
-        System.out.println(ClassLayout.parseInstance(user).toPrintable());
-        synchronized (user){
-            System.out.println("---------");
+    public static void main(String[] args) throws InterruptedException {
+        ClassHead head = new ClassHead();
+        User[] users = new User[2];
+        System.out.println(ClassLayout.parseInstance(head).toPrintable());
+        System.gc();
+        Thread.sleep(1000);
+        System.gc();
+        Thread.sleep(1000);
+        System.gc();
+        System.out.println(ClassLayout.parseInstance(head).toPrintable());
+
+
+        synchronized (head){
+            // 查看对象头信息 偏向锁等
+            System.out.println(ClassLayout.parseInstance(head).toPrintable());
         }
-        System.out.println(ClassLayout.parseInstance(user).toPrintable());
+        System.out.println(ClassLayout.parseInstance(users).toPrintable());
+    }
+
+    /**
+     * 对象头是对象实例创建后才会有的
+     */
+    public static class ClassHead {
+
     }
 }
--- src/com/eghm/reentrantlock/PriorityBlockingQueueTest.java
+++ src/com/eghm/reentrantlock/PriorityBlockingQueueTest.java
@@ -1,12 +1,13 @@
 package com.eghm.reentrantlock;
 
 import com.eghm.User;
+import com.google.common.collect.Lists;
 
 import java.util.concurrent.PriorityBlockingQueue;
 
 public class PriorityBlockingQueueTest {
     public static void main(String[] args) {
-        PriorityBlockingQueue<User> queue = new PriorityBlockingQueue<>();
-        queue.add(new User());
+        PriorityBlockingQueue<Integer> queue = new PriorityBlockingQueue<>(Lists.newArrayList(1,4,3,2,5,7,9,11));
+        System.out.println(queue);
     }
 }
